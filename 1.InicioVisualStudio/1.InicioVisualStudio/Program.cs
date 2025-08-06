504519f972076a13273575ca371a87cb1573c90d
mfvanegas <maria.vanegaso@upb.edu.co>
2025-08-06 09:35:30 -0500

    Variables, constantes y tipos de datos
    clase de lo del nombre y del iva

---
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1.InicioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración e inicialización de variables
+            string nombre = "Maria Fernanda";
+            const string iva = "19%";
+            string apellidos = "Vanegas Osorio";
+            Console.Write("Bienvenidos al curso de Fundamentos de Programcación\n");
+            nombre = "Maria Fernanda";
+            //iva= "20%"; No puede cambiar el valor de 'iva' por ser una constante
+            Console.Write("\t" + nombre + " " + apellidos + " " + iva);
+            Console.ReadKey();
+
+
+        }
+    }
+}
