79b501ca03856f5b1f107744fe674ddbdb2c8f95
mfvanegas <maria.vanegaso@upb.edu.co>
2025-08-29 09:34:34 -0500

    11.While3

---
+++ 11.While3/11.While3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _11.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados.*/
+
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("Ingrese números enteros positivos");
+            Console.WriteLine("Ingrese un número entero negativo para terminar");
+
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                suma += numero;
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"La suma total de los numeros ingresados es: {suma}");
+        }
+    }
+}
