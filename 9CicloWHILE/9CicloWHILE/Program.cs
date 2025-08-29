cc26b65861436654fe909d3120e0ce2dddc4e392
mfvanegas <maria.vanegaso@upb.edu.co>
2025-08-29 08:55:36 -0500

    9.CicloWHILE

---
+++ 9CicloWHILE/9CicloWHILE/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9CicloWHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine("La suma de los primeros 5 numeros enteros es:" + acumulador);
+        }
+    }
+}
