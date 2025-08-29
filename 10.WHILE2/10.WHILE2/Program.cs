5d50569ba11442197c812c87b737d84ab0531c77
mfvanegas <maria.vanegaso@upb.edu.co>
2025-08-29 09:19:21 -0500

    10.WHILE2

---
+++ 10.WHILE2/10.WHILE2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _10.WHILE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+
+            int contador = 1;
+            int factorial = 1;
+
+            while (contador <= 5)
+            {
+                factorial *= contador;
+                contador++;
+            }
+            Console.WriteLine($"El factorial de {5} es: {factorial}");
+        }
+    }
+}
