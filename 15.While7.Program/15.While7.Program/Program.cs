26ebe5ba8c0812b00459caaaf716a442535b3524
mfvanegas <maria.vanegaso@upb.edu.co>
2025-09-03 09:34:21 -0500

    aun falta terminarlo y falta el 16

---
+++ 15.While7.Program/15.While7.Program/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _15.While7.Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*15.Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero; 
+             * además, se quiere saber cuánto lleva ahorrado cada mes. */
+
+            int totalahorrado = 0;
+            int mes = 1;
+
+            while (mes <= 12)
+            {
+               Console.WriteLine("Ingerese la cantidad que desa depositare en el mes")
+            }
+        }
+    }
+}
