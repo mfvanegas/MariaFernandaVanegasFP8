62c51254ff564818d3fd66506f4933b2b18d01a2
mfvanegas <maria.vanegaso@upb.edu.co>
2025-09-03 09:18:59 -0500

    While6.Program

---
+++ 14.While6.Program/14.While6.Program/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _14.While6.Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*14. Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.*/
+
+            int rango = 0;
+            int contadorPares = 0;
+            int contador = 1;
+
+            Console.WriteLine("Ingrese el rango a evaluar");
+            rango=int.Parse(Console.ReadLine());
+
+            while (contador <= rango)
+            {
+                if (contador % 2 == 0)
+                {
+                    contadorPares++;
+                }
+                contador++;
+            }
+            Console.WriteLine($"La cantidad de números pares de 1 hasta {rango} es: {contadorPares}");
+        }
+    }
+}
