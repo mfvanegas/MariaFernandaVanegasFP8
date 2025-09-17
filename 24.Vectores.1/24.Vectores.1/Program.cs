9ee94054f54fa35bacf8a1559696b9855c771f22
mfvanegas <maria.vanegaso@upb.edu.co>
2025-09-17 09:34:35 -0500

    Falta terminar y el 25

---
+++ 24.Vectores.1/24.Vectores.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _24.Vectores._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
