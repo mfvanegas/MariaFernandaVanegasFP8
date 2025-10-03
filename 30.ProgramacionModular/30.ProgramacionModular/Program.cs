29ace96b70d9d7a0950e7f21992285fc173a4f4e
mfvanegas <maria.vanegaso@upb.edu.co>
2025-10-03 09:18:02 -0500

    Add files via upload

---
+++ 30.ProgramacionModular/30.ProgramacionModular/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _30.ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Mi nombre es Maria Fernanda");
+            //MostrarMensajes("Tengo 18 años");
+            //Console.WriteLine($"Edad calculada: {CalcularEdad()}");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2006)} años");
+        }
+
+        //Módulo 1 - Procedimiento sin parámetros
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenidos a la clase de Programación Modular");
+            MostrarMensajes("Estoy desde procedimiento sin parámetros");
+        }
+
+        //Módulo 2 - Procedimiento con parámetros
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Módulo 3 - Funciones sin parámetros
+        static int CalcularEdad()
+        {
+            int edad;
+            edad = 2025 - 2006;
+            return edad;
+        }
+
+        //Módulo 4 Función con parámetros
+        static int CalcularEdad(int añoActual, int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
